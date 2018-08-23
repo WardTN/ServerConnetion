1c26fb60d2225e89bd82c3632c13b4798ad88160
hovoh <863807776@qq.com>
2018-08-23 22:26:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chen/serverconnetion/DbOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.chen.serverconnetion;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "game_provider.db";
+    public static final String GAME_TABLE_NAME = "game";
+    private static final int DB_VERSION=1;
+    private String CREATE_GAME_TABLE="create table if not exists " + GAME_TABLE_NAME +"(_id integer primary key," + "name TEXT, "+"describe TEXT)";
+
+    public DbOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_GAME_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
